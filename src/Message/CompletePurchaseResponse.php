16b3605caffa5252b467de15ffdaec68d859f625
Ivan Kipyegon <ivan@bardo.com>
2016-10-26 15:56:28 +0300

    airtel money code change

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -10,40 +10,46 @@ use Omnipay\Common\Message\RequestInterface;
  */
 class CompletePurchaseResponse extends AbstractResponse
 {
-    public function __construct(RequestInterface $request, $data)
-    {
-        if (!is_array($data)) {
-            parse_str($data, $data);
-        }
+	public function __construct(RequestInterface $request, $data)
+	{
+		if (!is_array($data)) {
+			parse_str($data, $data);
+		}
 
-        parent::__construct($request, $data);
-    }
+		parent::__construct($request, $data);
+	}
 
-    public function isSuccessful()
-    {
-        
-		return isset($this->data['TransactionStatus']) &&  $this->data['TransactionStatus'] === "00" || $this->data['TransactionStatus']=== "M0"  || $this->data['TransactionStatus']=== "M2" || $this->data['TransactionStatus']=== "KO" || $this->data['TransactionStatus']=== "K2";
-    }
+	public function isSuccessful()
+	{
 
-    public function getTransactionReference()
-    {
-        return isset($this->data['SHOP_NUMBER']) ? $this->data['SHOP_NUMBER'] : null;
-    }
+		return isset($this->data['TransactionStatus']) &&  $this->data['TransactionStatus'] === "00" || $this->data['TransactionStatus']=== "M0"  || $this->data['TransactionStatus']=== "M2" || $this->data['TransactionStatus']=== "K0" || $this->data['TransactionStatus']=== "K2";
+	}
 
-    public function getMessage()
-    {
-        return isset($this->data['ResponseMessage']) ? $this->data['ResponseMessage'] : null;
-    }
+	public function getTransactionReference()
+	{
+		return isset($this->data['BardoNumber']) ? $this->data['BardoNumber'] : null;
+	}
+
+	public function getMessage()
+	{
+		return isset($this->data['ResponseMessage']) ? $this->data['ResponseMessage'] : null;
+	}
 	public function getTransactionDate()
-    {
-        return isset($this->data['TransactionDate']) ? $this->data['TransactionDate'] : null;
-		
-		
-    }
+	{
+		return isset($this->data['TransactionDate']) ? $this->data['TransactionDate'] : null;
+
+
+	}
 	public function getTransactionAmount()
-    {
-		
-        return isset($this->data['TransactionAmount']) ? $this->data['TransactionAmount'] : null;
-    }
-	
+	{
+
+		return isset($this->data['TransactionAmount']) ? $this->data['TransactionAmount'] : null;
+	}
+
+	public function getTransactionStatus()
+	{
+		return isset($this->data['TransactionStatus']) ? $this->data['TransactionStatus'] : null;
+
+	}
+
 }
